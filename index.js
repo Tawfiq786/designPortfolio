cf4c2bf4a96e3d4d8878bd810b4b93fde1448ff4
Tawfiq786 <95121422+Tawfiq786@users.noreply.github.com>
2024-04-10 23:14:20 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+
+const homeFunction= function(){
+    document.querySelector(".profile_main").style.opacity="1";
+    document.querySelector(".profile_main").style.transform="scale(1)";
+}
+
+
+const workFunction= function(){
+    document.querySelector(".entry_container").style.opacity="1";
+    document.querySelector(".entry_container").style.transform="translateX(0rem)";
+}
+
+const portFunction= function(){
+    document.querySelector(".entry_container").style.opacity="1";
+    document.querySelector(".entry_container").style.transform="translateX(0rem)";
+}
